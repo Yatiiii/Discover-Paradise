e41141ee44f4ee88f8434ef764b601e701266f9a
Yatiiii <yativishnoi23@gmail.com>
2021-10-25 23:18:13 +0530

    SECOND COMMIT

--- javascript/work.js
+++ javascript/work.js
@@ -4,8 +4,8 @@
 $(document).ready(function(){
     $('.mode').change(function(){
       if($(this).prop('checked')){
-        $('.header').css('background-image', "url('css/images/daya.jpg')");
-        $('.fwall').css('background-image', "url('css/images/dayb.jpg')");
+        $('.header').css('background-image', "url('CSS/images/daya.jpg')");
+        $('.fwall').css('background-image', "url('CSS/images/dayb.jpg')");
         $('body').css('background-color', "white");
         $('.mirado').css('color', "white");
         $('.card-body').css('background-color', "white");
@@ -13,8 +13,8 @@ $(document).ready(function(){
         $('p').css('color', "black");
       }
       else{
-        $('.header').css('background-image', "url('css/images/night.jpg')");
-        $('.fwall').css('background-image', "url('css/images/Mountain.jpg')");
+        $('.header').css('background-image', "url('CSS/images/night.jpg')");
+        $('.fwall').css('background-image', "url('CSS/images/Mountain.jpg')");
         $('body').css('background-color', "black");
         $('.card-body').css('background-color', "black");
         $('h3').css('color', "white");
